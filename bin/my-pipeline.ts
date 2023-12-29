1b8c0ee5d27c53a03db034dfe47ec187110e4c0f
Hossameldin Shahin <hshahin@vt.edu>
2023-12-29 10:47:52 -0500

    init

--- bin/my-pipeline.ts
+++ bin/my-pipeline.ts
@@ -3,6 +3,11 @@ import * as cdk from 'aws-cdk-lib';
 import { MyPipelineStack } from '../lib/my-pipeline-stack';
 
 const app = new cdk.App();
-new MyPipelineStack(app, 'MyPipelineStack');
+new MyPipelineStack(app, 'MyPipelineStack', {
+  env: {
+    account: '690901106489',
+    region: 'us-east-1',
+  }
+});
 
 app.synth();
