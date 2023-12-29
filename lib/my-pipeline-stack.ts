6beaef1f02e7425888ff324fe0042503626e8fd3
Hossameldin Shahin <hshahin@vt.edu>
2023-12-29 11:13:45 -0500

    init

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -16,19 +16,27 @@ export class MyPipelineStack extends cdk.Stack {
     });
 
     const wave = pipeline.addWave('wave');
-    const myAppEUStage = new MyPipelineAppStage(this, 'MyAppEU', {
+
+    const myAppEUW1Stage = new MyPipelineAppStage(this, 'MyAppEU', {
       env: { account: '690901106489', region: 'eu-west-1' }
     })
 
-    const myAppUSStage = new MyPipelineAppStage(this, 'MyAppUS', {
+    const myAppUSW1Stage = new MyPipelineAppStage(this, 'MyAppUS', {
       env: { account: '690901106489', region: 'us-west-1' }
     })
 
-    const myAppEUWave = wave.addStage(myAppEUStage);
-    myAppEUWave.addPre(new ManualApprovalStep('approval'));
+    const myAppUSE1StageDev = new MyPipelineAppStage(this, 'MyAppUSE1DEV', {
+      env: { account: '864571753663', region: 'us-east-1' }
+    })
 
-    const myAppUSWave = wave.addStage(myAppUSStage);
-    myAppUSWave.addPre(new ManualApprovalStep('approval'));
+    const myAppEUW1Wave = wave.addStage(myAppEUW1Stage);
+    myAppEUW1Wave.addPre(new ManualApprovalStep('approval'));
+
+    const myAppUSW1Wave = wave.addStage(myAppUSW1Stage);
+    myAppUSW1Wave.addPre(new ManualApprovalStep('approval'));
+
+    const myAppUSE1WaveDev = wave.addStage(myAppUSE1StageDev);
+    myAppUSE1WaveDev.addPre(new ManualApprovalStep('approval'));
   }
 }
 
