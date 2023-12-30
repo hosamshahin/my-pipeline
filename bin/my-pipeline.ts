f66b2f6f3f14a880157ded3c886131f4eae363d7
Hossameldin Shahin <hshahin@vt.edu>
2023-12-30 18:08:13 -0500

    init

--- bin/my-pipeline.ts
+++ bin/my-pipeline.ts
@@ -1,13 +1,38 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { MyPipelineStack } from '../lib/my-pipeline-stack';
+import { MyPipeline } from '../lib/my-pipeline-construct';
+
+const githubOrg = 'hosamshahin';
+const githubRepo = 'my-pipeline';
+const githubBranch = 'main';
+const region = 'us-east-1'
 
 const app = new cdk.App();
-new MyPipelineStack(app, 'MyPipelineStack', {
+
+const pipelineStack = new cdk.Stack(app, 'Pipeline', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION
   }
+});
+
+
+new MyPipeline(pipelineStack, 'Dev', {
+  account: cdk.SecretValue.secretsManager('DEV_ACCOUNT_ID').unsafeUnwrap().toString(),
+  region,
+  githubOrg,
+  githubRepo,
+  githubBranch: 'dev',
+  preApprovalRequired: false
+});
+
+new MyPipeline(pipelineStack, 'Prd', {
+  account: cdk.SecretValue.secretsManager('PRD_ACCOUNT_ID').unsafeUnwrap().toString(),
+  region,
+  githubOrg,
+  githubRepo,
+  githubBranch,
+  preApprovalRequired: true
 });
 
 app.synth();
---
+++ lib/my-pipeline-construct.ts
@@ -0,0 +1,58 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep, ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+import { MyPipelineAppStage } from './my-pipeline-app-stage';
+
+enum EnvIdName {
+  cicd = 'CICD_ACCOUNT_ID',
+  dev = 'DEV_ACCOUNT_ID',
+  stg = 'STG_ACCOUNT_ID',
+  prd = 'PRD_ACCOUNT_ID'
+}
+
+export interface MyPipelineProps {
+  // readonly deploymentEnv: string;
+  readonly account: string;
+  readonly region: string;
+  readonly githubOrg: string;
+  readonly githubRepo: string;
+  readonly githubBranch: string;
+  readonly preApprovalRequired: boolean | false;
+}
+
+export class MyPipeline extends Construct {
+  constructor(scope: Construct, id: string, props: MyPipelineProps) {
+    super(scope, id);
+
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      crossAccountKeys: true,
+      // pipelineName: `Pipeline-${props.deploymentEnv}`,
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub(`${props.githubOrg}/${props.githubRepo}`, props.githubBranch),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth'],
+      }),
+      codeBuildDefaults: {
+        buildEnvironment: {
+          environmentVariables: {
+            CICD_ACCOUNT_ID: { value: cdk.SecretValue.secretsManager('CICD_ACCOUNT_ID').unsafeUnwrap().toString() },
+            DEV_ACCOUNT_ID: { value: cdk.SecretValue.secretsManager('DEV_ACCOUNT_ID').unsafeUnwrap().toString() },
+            STG_ACCOUNT_ID: { value: cdk.SecretValue.secretsManager('STG_ACCOUNT_ID').unsafeUnwrap().toString() },
+            PRD_ACCOUNT_ID: { value: cdk.SecretValue.secretsManager('PRD_ACCOUNT_ID').unsafeUnwrap().toString() },
+            PRD_ACCOUNT_ID1: { value: cdk.SecretValue.secretsManager('PRD_ACCOUNT_ID').unsafeUnwrap().toString() },
+          }
+        }
+      }
+    });
+
+    const myApp = new MyPipelineAppStage(this, 'MyApp', {
+      env: { account: props.account, region: props.region }
+    })
+
+    const myStage = pipeline.addStage(myApp);
+
+    if (props.preApprovalRequired) {
+      myStage.addPre(new ManualApprovalStep('approval'));
+    }
+  }
+}
+
