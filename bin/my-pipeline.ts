6dca73fd0b9cbf3ac9f0435b20d312cdee835630
Hossameldin Shahin <hshahin@vt.edu>
2023-12-30 22:23:17 -0500

    init

--- bin/my-pipeline.ts
+++ bin/my-pipeline.ts
@@ -9,16 +9,21 @@ const region = 'us-east-1'
 
 const app = new cdk.App();
 
-const pipelineStack = new cdk.Stack(app, 'Pipeline', {
+const pipelineDev = new cdk.Stack(app, 'PipelineDev', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION
   }
 });
 
+const pipelinePrd = new cdk.Stack(app, 'PipelinePrd', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
+});
 
-
-new MyPipeline(pipelineStack, 'Dev', {
+new MyPipeline(pipelineDev, 'Dev', {
   deploymentEnv: 'dev',
   deploymentAcct: 'DEV_ACCOUNT_ID',
   account: cdk.SecretValue.secretsManager('DEV_ACCOUNT_ID').unsafeUnwrap().toString(),
@@ -29,15 +34,15 @@ new MyPipeline(pipelineStack, 'Dev', {
   preApprovalRequired: false
 });
 
-// new MyPipeline(pipelineStack, 'Prd', {
-//   deploymentEnv: 'prd',
-//   deploymentAcct: 'PRD_ACCOUNT_ID',
-//   account: cdk.SecretValue.secretsManager('PRD_ACCOUNT_ID').unsafeUnwrap().toString(),
-//   region,
-//   githubOrg,
-//   githubRepo,
-//   githubBranch,
-//   preApprovalRequired: true
-// });
+new MyPipeline(pipelinePrd, 'Prd', {
+  deploymentEnv: 'prd',
+  deploymentAcct: 'PRD_ACCOUNT_ID',
+  account: cdk.SecretValue.secretsManager('PRD_ACCOUNT_ID').unsafeUnwrap().toString(),
+  region,
+  githubOrg,
+  githubRepo,
+  githubBranch,
+  preApprovalRequired: true
+});
 
 app.synth();
